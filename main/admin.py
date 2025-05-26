4e74a9931fab7701596657b9d517f8e2e9481b79
BondarenkoKirill <bku089@gmail.com>
2025-05-26 23:38:45 +0300

    add ProjectListView to views.py

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from main.models import Position, Worker, Task, TaskType
+from main.models import Position, Worker, Task, TaskType, Project
 
 
 @admin.register(Position)
--- main/views.py
+++ main/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from main.models import Project
+
+
+class ProjectListView(ListView):
+    model = Project
+    template_name = "main/project_list.html"
+    context_object_name = "projects"
