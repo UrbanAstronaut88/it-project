0f058d57629f109d34cd7af880c1894589877a78
BondarenkoKirill <bku089@gmail.com>
2025-05-27 12:05:53 +0300

    added class HomeView in main/views.py

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, TemplateView
 
 from main.models import Project
 
@@ -8,3 +8,7 @@ class ProjectListView(ListView):
     model = Project
     template_name = "main/project_list.html"
     context_object_name = "projects"
+
+
+class HomeView(TemplateView):
+    template_name = "main/home.html"
