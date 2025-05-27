bfbd8352eb34bd367ff13bf4aa0033988b41d58f
BondarenkoKirill <bku089@gmail.com>
2025-05-27 19:24:42 +0300

    feat: created new route with name task-list

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ProjectListView, HomeView, WorkerListView
+from .views import ProjectListView, HomeView, WorkerListView, TaskListView
 
 app_name = "main"
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("projects/", ProjectListView.as_view(), name="project-list"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
 ]
