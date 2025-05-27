22726d75f28880bed85b66e99c02fd2d38078b33
BondarenkoKirill <bku089@gmail.com>
2025-05-27 12:11:46 +0300

    feat: created home.html to display the home page

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ProjectListView
+from .views import ProjectListView, HomeView
 
 
 app_name = "main"
 urlpatterns = [
+    path("", HomeView.as_view(), name="home"),
     path("projects/", ProjectListView.as_view(), name="project-list"),
 ]
