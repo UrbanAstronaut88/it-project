f08becde401100b5364f872e90869308d95308d0
BondarenkoKirill <bku089@gmail.com>
2025-05-27 19:19:08 +0300

    feat: added class TaskListView to main/views.py

--- main/views.py
+++ main/views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.shortcuts import render
 from django.views.generic import ListView, TemplateView
-
-from main.models import Project
+from main.models import Project, Task
 
 
 class ProjectListView(ListView):
@@ -15,9 +13,15 @@ class HomeView(TemplateView):
     template_name = "main/home.html"
 
 
-User = get_user_model()
 
+User = get_user_model()
 class WorkerListView(ListView):
     model = User
     template_name = "main/worker_list.html"
     context_object_name = "workers"
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "main/task_list.html"
+    context_object_name = "tasks"
