6fcbca43c82ee2ed3e1659545844afb23e365317
BondarenkoKirill <bku089@gmail.com>
2025-05-28 11:40:09 +0300

    feat: forms.py module created

---
+++ main/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Project
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ["name", "description"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "post"
+        self.helper.add_input(Submit("submit", "Save"))
