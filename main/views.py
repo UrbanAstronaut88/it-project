70d1e9752df478ccb556ff2dc812f5b7948c493c
BondarenkoKirill <bku089@gmail.com>
2025-05-28 21:32:26 +0300

    style: updated worker_confirm_delete.html template for better visualisation

--- main/views.py
+++ main/views.py
@@ -74,20 +74,20 @@ class WorkerDetailView(DetailView):
 
 class WorkerCreateView(CreateView):
     model = User
-    fields = ("username", "first_name", "last_name", "email", "position", "password")
+    fields = ("username", "first_name", "last_name", "email", "position")
     template_name = "main/worker_form.html"
     success_url = reverse_lazy("main:worker-list")
 
-    def form_valid(self, form):
-        user = form.save(commit=False)
-        user.set_password(form.cleaned_data["password"])
-        user.save()
-        return super().form_valid(form)
+    # def form_valid(self, form):
+    #     user = form.save(commit=False)
+    #     user.set_password(form.cleaned_data["password"])
+    #     user.save()
+    #     return super().form_valid(form)
 
 
 class WorkerUpdateView(UpdateView):
     model = User
-    fields = ("username", "first_name", "last_name", "email", "position", "password")
+    fields = ("username", "first_name", "last_name", "email", "position")
     template_name = "main/worker_form.html"
     success_url = reverse_lazy("main:worker-list")
 
