be56c4efe9d3cea7f6d19af36eaeff6250b9bde8
BondarenkoKirill <bku089@gmail.com>
2025-05-28 11:36:18 +0300

    feat: new route (project-detail) has been created to provide detailed information about the project

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,7 @@ app_name = "main"
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("projects/", ProjectListView.as_view(), name="project-list"),
+    path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
