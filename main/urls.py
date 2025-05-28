5157320f06f711c6c85da3e76877e1ba77f5c931
BondarenkoKirill <bku089@gmail.com>
2025-05-28 12:08:24 +0300

    feat: a new route named ‘project-create’ is created

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from .views import ProjectListView, HomeView, WorkerListView, TaskListView, TaskDetailView, TaskUpdateView, \
-    TaskDeleteView
+from .views import (
+    ProjectListView,
+    HomeView,
+    WorkerListView,
+    TaskListView,
+    TaskDetailView,
+    TaskUpdateView,
+    TaskDeleteView,
+    ProjectDetailView, ProjectCreateView,
+)
 
 app_name = "main"
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("projects/", ProjectListView.as_view(), name="project-list"),
     path("projects/<int:pk>/", ProjectDetailView.as_view(), name="project-detail"),
+    path("projects/create/", ProjectCreateView.as_view(), name="project-create"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
