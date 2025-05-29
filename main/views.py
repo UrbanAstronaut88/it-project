bd7c77011b65a740878b9bca11657e911d98612c
BondarenkoKirill <bku089@gmail.com>
2025-05-29 21:57:15 +0300

    updated WorkerDetailView, added to the context of the list of tasks assigned to this employee

--- main/views.py
+++ main/views.py
@@ -68,7 +68,8 @@ class WorkerDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["tasks"] = Task.objects.filter(assignees=self.object)
+        worker = self.object
+        context["assigned_tasks"] = worker.tasks.all()
         return context
 
 
