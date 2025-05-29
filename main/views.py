6ac1d01d15d2cd5aef0fb891c46b2102b5ba55f2
BondarenkoKirill <bku089@gmail.com>
2025-05-29 21:51:47 +0300

    TaskListView filtering by task status removed

--- main/views.py
+++ main/views.py
@@ -107,14 +107,11 @@ class TaskListView(ListView):
         queryset = Task.objects.select_related("project").prefetch_related("assignees")
         project_id = self.request.GET.get("project")
         assignee_id = self.request.GET.get("assignee")
-        status = self.request.GET.get("status")
 
         if project_id:
             queryset = queryset.filter(project_id=project_id)
         if assignee_id:
             queryset = queryset.filter(assignees__id=assignee_id)
-        if status:
-            queryset = queryset.filter(status=status)
         return queryset.distinct()
 
     def get_context_data(self, **kwargs):
@@ -123,7 +120,6 @@ class TaskListView(ListView):
         context["workers"] = Worker.objects.all()
         context["selected_project_id"] = self.request.GET.get("project", "")
         context["selected_assignee_id"] = self.request.GET.get("assignee", "")
-        context["selected_status"] = self.request.GET.get("status", "")
         return context
 
 
